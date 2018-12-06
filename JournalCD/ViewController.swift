7564e714ad58e660a4ace67fad90633e6e276069
Trevor Adcock <trevor.adcock@mac.com>
2018-12-06 09:19:25 -0700

    Initial Commit

---
+++ JournalCD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JournalCD
+//
+//  Created by DevMountain on 12/6/18.
+//  Copyright © 2018 trevorAdcock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
